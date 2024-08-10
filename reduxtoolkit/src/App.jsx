48b5ec2e83853de8ca4b462e5794e9b0d2985ef1
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-10 11:58:34 +0530

    add Addtodo and removetodo functionality

--- reduxtoolkit/src/App.jsx
+++ reduxtoolkit/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import AddToDo from "./components/AddToDo";
+import Todo from "./components/Todo";
 function App() {
   return (
     <>
       <h1>Learn About ReduxToolKit</h1>
+      <AddToDo />
+      <Todo />
     </>
   );
 }
--- reduxtoolkit/src/features/todo/todoSlice.js
+++ reduxtoolkit/src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: [{ id: 1, text: "Hello World" }],
+  todos: [{ id: 1, text: "Hello world" }],
 };
 
 export const todoSlice = createSlice({
@@ -18,13 +18,9 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
-    updateTodo: (state, action) => {
-      const todo = state.todos.find((todo) => todo.id === action.payload.id);
-      todo.text = action.payload.text;
-    },
   },
 });
 
-export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+export const { addTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
